97a9c5ac65ebfd541d7dad8457984e16e1967e4d
Tofixrs <73693639+Tofixrs@users.noreply.github.com>
2026-04-07 22:25:35 +0200

    feat: update run_docker.sh to kill and delete old container

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker container kill scrapbot_container
+docker container remove scrapbot_container
 docker build -t scrapbot:latest .
 docker volume create scrapbot-data || true
 docker run -d -v scrapbot-data:/app/data --restart unless-stopped --name scrapbot_container scrapbot:latest
